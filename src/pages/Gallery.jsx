17dc220e183e8cac26b89323d3b5989c575765a1
mojeed-painless <shittumjd00@gmail.com>
2026-01-03 19:13:38 +0100

    Worked on the LightBox for Gallery

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import FilterButton from '../components/FilterButton';
 import '../assets/styles/gallery.css';
 import { mainGallery } from '../data';
@@ -6,14 +6,23 @@ import { FaPhotoFilm } from "react-icons/fa6";
 import { RowsPhotoAlbum } from "react-photo-album";
 import { MdCancel } from "react-icons/md";
 import { FiFilter } from "react-icons/fi";
-import "react-photo-album/rows.css";
+import Lightbox from "yet-another-react-lightbox";
+import Counter from "yet-another-react-lightbox/plugins/counter";
+import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
+import "yet-another-react-lightbox/styles.css";
+import "yet-another-react-lightbox/plugins/counter.css";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
 
 export default function Gallery() {
 
     const [galleryId, setGalleryId] = useState(0);
     const [activeAlbum, setActiveAlbum] = useState(true);
     const [selectedCategory, setSelectedCategory] = useState('All');
-    const [activeFilter, setActiveFilter] = useState(false)
+    const [activeFilter, setActiveFilter] = useState(false);
+    const [lightboxOpen, setLightboxOpen] = useState(false);
+    const [lightboxIndex, setLightboxIndex] = useState(0);
+
+    const thumbnailsRef = useRef(null);
 
     function handleFilterClick(category) {
         setSelectedCategory(category);
@@ -22,11 +31,17 @@ export default function Gallery() {
 
     function handleSelect(selectedId) {
         setGalleryId(selectedId);
-        setActiveAlbum(aa => !aa);
+        // setActiveAlbum(aa => !aa);
+        setLightboxOpen(true);
     }
 
     function handleActiveState() {
         setActiveAlbum(aa => !aa);
+    }
+
+    function handleLightboxOpen(index) {
+        setLightboxIndex(index);
+        setLightboxOpen(true);
     }
 
     const selectedGallery = mainGallery.find((gallery) => (
@@ -71,18 +86,33 @@ export default function Gallery() {
                     ))}
                 </div>
 
-                <div className={`gallery__album ${!activeAlbum ? 'active-gallery__album' : ""}`}>
+                {/* <div className={`gallery__album ${!activeAlbum ? 'active-gallery__album' : ""}`}>
                     <small onClick={handleActiveState}><MdCancel /></small>
                     {selectedGallery && (
                         <>
                             <p>{selectedGallery.caption}</p>
                             <RowsPhotoAlbum 
                                 photos={selectedGallery.images} 
+                                onClick={({ index }) => handleLightboxOpen(index)}
                             />
                         </>
                     )}
-                </div>
+                </div> */}
             </div>
+
+            <Lightbox
+                open={lightboxOpen}
+                close={() => setLightboxOpen(false)}
+                slides={selectedGallery ? selectedGallery.images : []}
+                index={lightboxIndex}
+                plugins={[Counter, Thumbnails]}
+                counter={{ container: { style: { top: "unset", bottom: 0 } } }}
+
+                thumbnails={{ ref: thumbnailsRef }}
+                on={{click: () => {
+                    (thumbnailsRef.current?.visible ? thumbnailsRef.current?.hide : thumbnailsRef.current?.show)?.();
+                }}}
+            />
         </article>
     )
 }
